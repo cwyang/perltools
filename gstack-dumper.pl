584d0d11507c3513c6770601e424576496ad8de9
Chul-Woong Yang <cwyang@gmail.com>
2020-03-20 23:41:20 +0900

    track threads

--- gstack-dumper.pl
+++ gstack-dumper.pl
@@ -26,7 +26,7 @@ my $d;
 $outdir = "/var/log/gstack";
 $interval = 5;
 $start_at = 3;
-$max_num = 3;
+$max_num = 5;
 $threshold = 95;
 
 GetOptions(
@@ -44,13 +44,13 @@ Usage: $progname [options] program_to_watch
 Options:
   --outdir <dir>	directory to dump gstack results
   --interval <minutes>	cpu utilization check interval in minutes	(def: 5)
-  --start <num>		number of consecutive events to start dump	(def: 3)
-  --max <num>		maximum number of dump files for each process	(def: 3)
+  --start <num>		number of consecutive high-loads to start dump	(def: 3)
+  --max <num>		maximum number of dump files for each thread	(def: 5)
   --threshold <num>	CPU utilization to trigger event		(def: 95)
   --help		print this help
 
 The command tracks given programs and dumps gstack result of running process
-when the process cpu utilization is over 95% for specified periods.
+when the thread cpu utilization is over 95% for specified periods.
 EOT
     exit 0;
 }
@@ -63,35 +63,47 @@ die "ps not found\n" unless prog_exists("ps");
 
 die "cannot write to $outdir\n" unless mkdir_p("$outdir");
 
-print "\u$progname start monitoring <$prog_to_watch> each $interval minutes.\n";
-print "When $start_at consecutive high load (CPU >= $threshold%) events are detected,"
-    . " it stores gstack dumpfiles to $outdir, upto $max_num per each process.\n";
+print "\u$progname starts monitoring <$prog_to_watch> each $interval minutes.\n";
+print "When $start_at consecutive high load (CPU >= $threshold%) threads are detected,"
+    . " it calls gstack $max_num times with 1 minute interval and logs the results.\n";
 
 my $cmd = "ps -C $prog_to_watch -Lo pcpu,psr,pid,tid,cputime,s,comm\n";
-my %procs;
+my (%procs, %dump_count);
 
 for (;;) {
     my @output = `$cmd`;
     my $date = `date`;
     my %targets = filter_target(@output);
-    print @output;
-    
-    for my $pid (keys %procs) {
-	delete $procs{$pid} unless exists $targets{$pid} 
+    die "\u$progname stops monitoring <$prog_to_watch>"
+	if getppid == 1;	# calling script dies
+
+    for my $tid (keys %procs) {
+	delete $procs{$tid} unless exists $targets{$tid} 
     }
-    for my $pid (keys %targets) {
-	if (++$procs{$pid} >= $start_at) {
-	    dump_gstack($pid, $date, \@output);
+
+    my $dumped = 0;
+    for my $tid (keys %targets) {
+	if (($dump_count{$tid} // 0) >= $max_num) {
+	    next;
+	}
+	if (++$procs{$tid} >= $start_at) {
+	    dump_gstack($targets{$tid}, $date, \@output, ++$dump_count{$tid});
+	    $dumped = 1;
 	}
     }
-    sleep 5;
+    if ($dumped) {
+	sleep 60;
+    } else {
+	sleep $interval * 60;
+    }
 }
 #print "hi $prog $prog2\n"
 
 sub filter_target {
     shift;
     map {
-	(split ' ')[2] => 1;	# pid
+	my @a = split ' ';
+	$a[3] => $a[2];	# (tid => pid)
     }
     grep {
 	(split ' ')[0] >= $threshold;
@@ -102,12 +114,12 @@ sub filter_target {
 }
 
 sub dump_gstack {
-    my ($pid, $date, $output_ref) = @_;
+    my ($pid, $date, $output_ref, $no) = @_;
     my $gstack = `gstack $pid`;
     my $dest = $prog_to_watch;
     $dest =~ s|/|_|g;
 
-    print "dumping high-load process info to $outdir/$dest.$pid.log\n";
+    print "dumping high-load process info to $outdir/$dest.$pid.log ($no/$max_num)\n";
     if (open DUMPFILE, ">>", "$outdir/$dest.$pid.log") {
 	print DUMPFILE $date;
 	print DUMPFILE @$output_ref;
